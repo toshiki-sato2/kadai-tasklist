95337a162c29c73dcf2a854fd3f60274ab91e35d
toshiki.sato <toshiki-sato2@dac.co.jp>
2024-05-28 01:30:51 +0000

    laravel breeze daisyui

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TasksController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +14,18 @@ use App\Http\Controllers\TasksController;
 |
 */
 
-/*無名関数としてfunctionの定義*/
-/*/にGET命令が来たら、それはwelcome.blade.phpに飛ばせよ*/
-/*view下のwelcome.blade.phpへ接続する、階層が深くなったら*.hogehoge.phpとドットで*/
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
-});*/
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/', [TasksController::class, 'index']);
-Route::resource('tasks', TasksController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
